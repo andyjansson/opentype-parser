6421d62ac89004ad9e8adba6d21a9993a1be5d8e
Bogdan Chadkin <trysound@yandex.ru>
2015-10-23 00:16:28 +0300

    Added .editorconfig

--- index.js
+++ index.js
@@ -3,20 +3,16 @@ var os2 = require('./lib/os2');
 
 module.exports = function(data) {
 	var sfntVersion = data.readInt32BE(0);
-	
-	if (
-		(sfntVersion !== 0x00010000) &&
-		(sfntVersion !== 0x4F54544F)
-		)
-	{
+
+	if (sfntVersion !== 0x00010000 && sfntVersion !== 0x4F54544F) {
 		return Promise.reject();
 	}
-	
+
 	var openType = {
 		version: sfntVersion,
-		type: (sfntVersion === 0x00010000 ? 'TTF' : 'OTF')
+		type: sfntVersion === 0x00010000 ? 'TTF' : 'OTF'
 	};
-	
+
 	var numTables = data.readUInt16BE(4);
 	for (var i = 0; i < numTables; i++) {
 		var tag = data.readUInt32BE(12 + 16 * i);
